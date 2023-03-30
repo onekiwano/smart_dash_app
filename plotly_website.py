4aa1d702f8dab5f4235f598ec7a1b07308097c61
Emmanuel CALVET <emmanuel.calvet@usherbrooke.ca>
2023-03-29 23:14:53 -0400

    Update

--- plotly_website.py
+++ plotly_website.py
@@ -8,17 +8,17 @@ Created on Sun Mar 26 16:03:01 2023
 import numpy as np
 import os 
 from datetime import datetime
-import dash_bootstrap_components as dbc
 
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 # https://towardsdatascience.com/how-to-create-a-plotly-visualization-and-embed-it-on-websites-517c1a78568b
+# https://dash.plotly.com/layout
 # Import libraries
 from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import plotly.express as px
 
-data_end = {'now':'2023-03-25-20-20-20'}
+data_end = {'now':'2023-03-16-20-20-20'}
 
 FILENAME = os.path.realpath(__file__)
 CDIR = os.path.dirname(FILENAME)
@@ -28,71 +28,112 @@ os.makedirs(CRYPTODIR, exist_ok=True)
 
 
 dca_strategies_name = ['Standard DCA',
-                  'Smart DCA: contained',
-                  'Smart DCA: unlished',
-                 ] 
+                       'Smart DCA',
+                       ] 
 
 dca_strategies = ['dca_1',
                   'sigmoid',
-                  'dca_ratio'
                  ] 
 
 dic_strategy_names = {dca_strategies_name[i]:dca_strategies[i] for i in range(len(dca_strategies))}
 
 # Create the Dash app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = Dash(__name__, external_stylesheets=external_stylesheets,
-           suppress_callback_exceptions=True,
-           prevent_initial_callbacks=True)
+app = Dash(__name__)
+
+# , external_stylesheets=external_stylesheets,
+#            suppress_callback_exceptions=True,
+#            prevent_initial_callbacks=True)
 
 # Set up the app layout
-title = dcc.Markdown(children='# DCA simulations')
+title = html.H1('DCA calculation', style={'color':'darkorange', 
+                                          'fontsize':40,
+                                          'text-align':'center'})
+decription_text = 'Welcome to the DCA and smart DCA calculator, in here you can check how awesome are our algos!'
 cum_ret = dcc.Markdown(children='')
-base_value = dbc.Input(value=25, min=0)
+base_value = dcc.Input(value=25, min=0)
 
 dca_dropdown = dcc.Dropdown(options=dca_strategies_name,
                             value='Standard DCA',
                             clearable=False)
 list_years = [(2018 + i) for i in range(6)]
-startdate_dropdown = dcc.Dropdown(list_years, value=2022, clearable=False)
+startdate_dropdown = dcc.Dropdown(list_years, value=2018, clearable=False)
 enddate_dropdown = dcc.Dropdown(list_years, value=2023, clearable=False)
-window_slider = dcc.Slider(50, 300, step=50, value=50, 
+window_slider = dcc.Slider(50, 400, step=50, value=400, 
                            id='window-slider')
 period_slider = dcc.Slider(1, 20, step=3, value=1, id='period-slider')
-window_slider = dcc.Slider(50, 300, step=50, value=50, 
-                           id='window-slider')
-graph_price = dcc.Graph(figure={})
-graph_amount = dcc.Graph(figure={})
 
-disp = lambda title, obj, **kwargs: html.Div(children=[html.H1(children=title),
+
+time = 'now'
+end_date = data_end[time]
+crypto_pair = 'BTCUSDT'
+crypto_df = pd.read_csv(f"results/{time}_{end_date}_{crypto_pair}_dca_1.csv")
+crypto_df = crypto_df.dropna()
+
+# Plot the price
+price_fig = px.line(crypto_df,
+                    x='Date', y='Close',
+                    title=f'{crypto_pair[:3]} price in USD',
+                    labels={"Close": "Price ($)"})
+
+# plot amount boughts over time
+amt_fig = px.line(crypto_df,
+                  x='Date', y='Amounts',
+                  title=f'Amounts bought for Standard DCA strategy',)
+
+
+
+
+
+graph_price = dcc.Graph(figure=price_fig)
+graph_amount = dcc.Graph(figure=amt_fig)
+
+disp = lambda title, obj, **kwargs: html.Div(children=[html.H3(children=title),
                                              obj], 
-                                             style={"width":"100%"},
                                              **kwargs)
 
-app.layout = dbc.Container([
-                    dbc.Row([
-                    dbc.Col([disp('', title)], width=6)], 
-                    justify='center'),
+app.layout = html.Div([
+                    # html, title of the page
+                    title,
+                    
+                    # Description
+                    disp('', dcc.Markdown(decription_text), style={'text-align':'center'}),
                     html.Br(),
-                    dbc.Row([
-                    dbc.Col([disp('Start year', startdate_dropdown)], width=6),
-                    dbc.Col([disp('End year', enddate_dropdown)], width=6),
-                    ]),
+                    #First row (side by side)
+                    html.Div(children=[
+                        html.Div(children=[
+                            disp('Start year', startdate_dropdown, style={"width":"50%"}),
+                            ], style={'left':80, 'position':'relative', 'flex': 1}),
+                        html.Div(children=[
+                            disp('End year', enddate_dropdown, style={"width":"50%"}),
+                            ], style={'right':50, 'position':'relative', 'flex': 1}),
+                    ], style={'display': 'flex', 'flex-direction': 'row'}),
                     html.Br(),
-                    dbc.Row([
-                    dbc.Col([disp('', graph_price)], width=12),
-                    ]),
-                    dbc.Row([
-                    dbc.Col([disp('', graph_amount)], width=8),
-                    dbc.Col([disp('Strategy', dca_dropdown),
-                             disp('Window length (day)', window_slider),
-                             disp('Period (day)', period_slider),
-                             disp('Maximum per day ($)', base_value)], width=4),
-                    ]),
-                    html.Br(),
-                    dbc.Row([
-                    dbc.Col([disp('Summary:', cum_ret)], width=12),
-                    ]),
+                    ## Price graph
+                    disp('', graph_price),
+                    # Second row (side by side)
+                    html.Div(children=[
+                        html.Div(children=[
+                            disp('Summary:', cum_ret, style={"width":"120%"}),
+                            ], style={'left':70, 'position':'relative', 'flex': 1}),
+                        html.Div(children=[
+                                  disp('Strategy', dca_dropdown, style={"width":"40%"}),
+                              ], style={'left':80, 'position':'relative', 'flex': 1}),
+                        html.Div(children=[
+                            disp('Basis per day ($)', base_value, style={"width":"60%"}), 
+                            ], style={'right':50, 'position':'relative', 'flex': 1}),
+                    ], style={'display': 'flex', 'flex-direction': 'row'}),
+                    # Graph of buy
+                    disp('', graph_amount),
+                    # Third row (side by side)
+                    html.Div(children=[
+                        html.Div(children=[
+                            disp('Window length (day)', window_slider, style={"width":"80%"}),
+                            ], style={'left':50, 'position':'relative', 'flex': 1}),
+                        html.Div(children=[
+                            disp('Period (day)', period_slider, style={"width":"80%"}),
+                            ], style={'flex': 1}),
+                        ], style={'display': 'flex', 'flex-direction': 'row'}),
                 ])                     
 
 crypto_pair = 'BTCUSDT'
@@ -108,7 +149,7 @@ crypto_pair = 'BTCUSDT'
     Input(component_id=startdate_dropdown, component_property='value'),
     Input(component_id=enddate_dropdown, component_property='value'),
 )
-def update_graph(base, period, window, title_strat='Standard DCA', start_year=2022, end_year=2023):
+def update_graph(base=25, period=1, window=50, title_strat='Standard DCA', start_year=2022, end_year=2023):
     # Load the dataset
     time = 'now'
     name_strat = dic_strategy_names[title_strat]
@@ -138,14 +179,14 @@ def update_graph(base, period, window, title_strat='Standard DCA', start_year=20
     total_boughts = crypto_boughts.sum()
     wealth = total_boughts * crypto_df['Close'].values[-1]
     total_budget = crypto_df['Amounts'].sum()
-    cm = np.round(wealth/total_budget, 3)
+    roi = np.round((wealth - total_budget)/total_budget, 3) # Return of Investment
     total_budget = np.round(total_budget, 1)
     crypto_boughts = np.round(total_boughts, 4)
     wealth = np.round(wealth, 1)
     avg_price = np.round(total_budget/crypto_boughts, 1)
-    summary = f"|CM| |Wealth| |Spent| |Crypto| |Average price|\n \
+    summary = f"|ROI| |Wealth| |Spent| |Crypto| |Average price|\n \
            |-------|--|-------|--|-------|--|-------|--|-----|\n \
-           |{cm}| |{wealth}$| |{total_budget}$| |{crypto_boughts}| |{avg_price}$|"
+           |{roi}| |{wealth}$| |{total_budget}$| |{crypto_boughts}| |{avg_price}$|"
     
     
     # Plot the price
@@ -163,4 +204,4 @@ def update_graph(base, period, window, title_strat='Standard DCA', start_year=20
 
 # Run local server
 if __name__ == '__main__':
-    app.run_server(debug=False, port=8060, host='0.0.0.0')
+    app.run_server(debug=False, port=8200, host='0.0.0.0')
